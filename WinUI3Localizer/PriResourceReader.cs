dfbda6c41fda622b9b40d4fea9862161244247b1
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2024-04-12 23:30:50 +0900

    Format code for consistency

--- WinUI3Localizer/PriResourceReader.cs
+++ WinUI3Localizer/PriResourceReader.cs
@@ -1,12 +1,10 @@
 using Microsoft.Windows.ApplicationModel.Resources;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WinUI3Localizer;
+
 internal class PriResourceReader
 {
     private readonly ResourceManager resourceManager;
@@ -18,29 +16,30 @@ internal class PriResourceReader
 
     public IEnumerable<LanguageDictionary.Item> GetItems(string language, string subTreeName = "Resources")
     {
-        if (string.IsNullOrEmpty(subTreeName) || subTreeName == "/")
+        if (string.IsNullOrEmpty(subTreeName) is true ||
+            subTreeName == "/")
         {
             subTreeName = "Resources";
         }
-        else if (subTreeName.EndsWith('/'))
+        else if (subTreeName.EndsWith('/') is true)
         {
             subTreeName = subTreeName[..^1];
         }
 
         ResourceMap resourceMap = this.resourceManager.MainResourceMap.TryGetSubtree(subTreeName);
-        if (resourceMap != null)
+
+        if (resourceMap is not null)
         {
             ResourceContext resourceContext = this.resourceManager.CreateResourceContext();
             resourceContext.QualifierValues[KnownResourceQualifierName.Language] = language;
 
-            return GetItemsCore(resourceMap, subTreeName, resourceContext);
+            return PriResourceReader.GetItemsCore(resourceMap, subTreeName, resourceContext);
         }
 
         return Enumerable.Empty<LanguageDictionary.Item>();
     }
 
-
-    private IEnumerable<LanguageDictionary.Item> GetItemsCore(ResourceMap resourceMap, string subTreeName, ResourceContext resourceContext)
+    private static IEnumerable<LanguageDictionary.Item> GetItemsCore(ResourceMap resourceMap, string subTreeName, ResourceContext resourceContext)
     {
         bool isResourcesSubTree = string.Equals(subTreeName, "Resources", StringComparison.OrdinalIgnoreCase);
         uint count = resourceMap.ResourceCount;
@@ -49,49 +48,18 @@ internal class PriResourceReader
         {
             (string key, ResourceCandidate? candidate) = resourceMap.GetValueByIndex(i, resourceContext);
 
-            if (candidate != null && candidate.Kind == ResourceCandidateKind.String)
+            if (candidate is not null &&
+                candidate.Kind is ResourceCandidateKind.String)
             {
                 key = key.Replace('/', '.');
+
                 if (!isResourcesSubTree)
                 {
                     key = $"/{subTreeName}/{key}";
                 }
+
                 yield return LocalizerBuilder.CreateLanguageDictionaryItem(key, candidate.ValueAsString);
             }
         }
     }
-
 }
-
-internal class PriResourceReaderFactory
-{
-    private readonly Dictionary<string, PriResourceReader> readers = new Dictionary<string, PriResourceReader>();
-
-    internal PriResourceReader GetPriResourceReader(string? priFile)
-    {
-        string? normalizedFilePath = string.Empty;
-
-        if (!string.IsNullOrEmpty(priFile))
-        {
-            normalizedFilePath = System.IO.Path.GetFullPath(priFile);
-        }
-
-        if (!this.readers.TryGetValue(normalizedFilePath, out PriResourceReader? reader))
-        {
-            ResourceManager manager;
-            if (string.IsNullOrEmpty(normalizedFilePath))
-            {
-                manager = new ResourceManager();
-            }
-            else
-            {
-                manager = new ResourceManager(normalizedFilePath);
-            }
-            reader = new PriResourceReader(manager);
-            this.readers[normalizedFilePath] = reader;
-        }
-
-        return reader;
-    }
-}
-
